8aee163d87d1e89e1bb82aa61a748874d88a5427
Bemuseed <calebmullan@hotmail.com>
2023-03-03 18:40:55 +0000

    Simplified tensor logic

--- tensor_manager.py
+++ tensor_manager.py
@@ -6,7 +6,7 @@ def get_tensors(dct):
     new_dict = {'state_dict': dict()}
     for k in list(dct['state_dict'].keys()):
         new_dict['state_dict'][k] = 0
-    return tensors, new_dict
+    return [t.numpy() for t in tensors], new_dict
 
 def restore_tensor_dict(tensors, dict_template):
     t_counter = 0
@@ -16,11 +16,6 @@ def restore_tensor_dict(tensors, dict_template):
        t_counter += 1
     return new_dict
 
-
-def get_tensors_as_lists(d):
-    tensors, dict_template = get_tensors(d)
-    return [t.numpy() for t in tensors], dict_template
-
 def get_tensors_from_file(ckpt_file):
     tensor_dict = torch.load(ckpt_file, map_location="cpu")
-    return get_tensors_as_lists(tensor_dict)
+    return get_tensors(tensor_dict)
